248e82d497390e7daf6585bf8cd0a34d848fe129
Purtha Shaariyaar <92152517+PurthaShaariyaar@users.noreply.github.com>
2024-03-25 05:38:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,26 +3,29 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
 
-
 const app = express();
-const PORT = 4000
+const PORT = 4004;
 
 // Load combined data
 const combinedData = require('./combined_data_copy.json');
 
-// Function to get team data
+// Function to retrieve team data
 const getTeamData = (teamName) => {
     const teamDataFiltered = combinedData.filter(data => data.team === teamName || data.team_opp === teamName);
     if (teamDataFiltered.length === 0) {
         throw new Error(`No data found for team: ${teamName}`);
     }
-    const numericColumns = ['HTR', 'Acc', 'Cons', 'FH', 'TRI', 'PC', 'player_weight'];
+
+    const numericColumns = ['goals_scored', 'shots_on_goal', 'save_percentage', 'faceoff_win_percentage', 'power_play_goals', 'power_play_opportunities', 'shots_on_goal_opp', 'penalty_kill_goals', 'player_weight'];
+
     const teamDataNumeric = teamDataFiltered.map(data => numericColumns.map(col => data[col]));
     const teamDataMean = numericColumns.reduce((acc, col, index) => {
         acc[col] = teamDataNumeric.reduce((sum, data) => sum + data[index], 0) / teamDataNumeric.length;
         return acc;
     }, {});
+
     const averagePlayerWeight = combinedData.filter(data => data.Tm === teamName).reduce((sum, data) => sum + data.player_weight, 0) / combinedData.filter(data => data.Tm === teamName).length;
+
     return { teamDataMean, averagePlayerWeight };
 };
 
@@ -31,18 +34,23 @@ const predictGameOutcome = (team1Name, team2Name) => {
     const { teamDataMean: team1Data, averagePlayerWeight: team1PlayerWeight } = getTeamData(team1Name);
     const { teamDataMean: team2Data, averagePlayerWeight: team2PlayerWeight } = getTeamData(team2Name);
 
-    const featuresTeam1 = ['HTR', 'Acc', 'Cons', 'FH', 'TRI', 'PC', 'player_weight'].map(col => team1Data[col]).concat([team1PlayerWeight]);
-    const featuresTeam2 = ['HTR', 'Acc', 'Cons', 'FH', 'TRI', 'PC', 'player_weight'].map(col => team2Data[col]).concat([team2PlayerWeight]);
+    // Prepare features for scaling
+    const featuresTeam1 = ['goals_scored', 'shots_on_goal', 'save_percentage', 'faceoff_win_percentage', 'power_play_goals', 'power_play_opportunities', 'shots_on_goal_opp', 'penalty_kill_goals', 'player_weight'].map(col => team1Data[col]);
+    const featuresTeam2 = ['goals_scored', 'shots_on_goal', 'save_percentage', 'faceoff_win_percentage', 'power_play_goals', 'power_play_opportunities', 'shots_on_goal_opp', 'penalty_kill_goals', 'player_weight'].map(col => team2Data[col]);
 
-    const inputFeatures = tf.tensor2d([featuresTeam1, featuresTeam2]);
+    // Assuming you have a model object for prediction
+    const predictionTeam1 = model.predict([featuresTeam1, team1PlayerWeight]);
+    const predictionTeam2 = model.predict([featuresTeam2, team2PlayerWeight]);
 
-    const predictions = model.predict(inputFeatures).arraySync();
-
-    if (predictions[0] > 0.5) {
-        return `${team1Name} is predicted to win against ${team2Name}`;
+    // Interpret the prediction
+    let winner;
+    if (predictionTeam1[0] > 0.5) {
+        winner = team1Name;
     } else {
-        return `${team2Name} is predicted to win against ${team1Name}`;
+        winner = team2Name;
     }
+
+    return `${winner} is predicted to win`;
 };
 
 // Route to predict game outcome
